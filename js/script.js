e3633eeb24c84d11696de4780e1e849777a846a5
SteadFast <sfast0258@gmail.com>
2024-05-14 14:10:27 +0100

    Write the logic to get the human choice

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ function getComputerChoice() {
   // return the selected string
   return selectedString;
 }
+
+function getHumanChoice() {
+  //1- get user input
+  const userChoice = prompt(`Select a string below and enter it : -
+  rock
+  paper
+  scissors`).toLowerCase();
+
+  //2- declare a list of strings containing "rock","paper,"scissors"
+  const CHOICES = ['rock', 'paper', 'scissors'];
+
+  //3- check if the user string matches one of the strings in (2)
+  if (CHOICES.includes(userChoice)) {
+    return userChoice;
+  }
+
+  //4- prompt back the user to make the correct choice
+  return getHumanChoice();
+}
