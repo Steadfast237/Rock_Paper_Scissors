61aa2072f41c012c1beb8164b5b7728c0a41d035
SteadFast <sfast0258@gmail.com>
2024-05-14 13:19:05 +0100

    Write the logic to get the computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+function getComputerChoice() {
+  // declare a list of strings containing "rock","paper,"scissors"
+  const CHOICES = ['rock', 'paper', 'scissors'];
+
+  // randomly select a string from the list
+  const randomNumber = Math.floor(Math.random() * CHOICES.length);
+  const selectedString = CHOICES[randomNumber];
+
+  // return the selected string
+  return selectedString;
+}
